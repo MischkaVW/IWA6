e0bdb5db89859e397ec0100e8498ef594dc86a28
Mischka van Wyk <127859454+MischkaVW@users.noreply.github.com>
2023-05-10 22:04:26 +0200

    challenge 2 commit

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
-const primaryPhone = 'O748105141'
-const secondaryPhone = '0219131568'
+const rent = 400;
+const tax = '8%';
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00;
+const minuteOfDay = 00;
 
 // Only change below this line
 
-const primaryValid = primaryPhone == typeof number
+parseInt(tax)
 
-const secondaryValid = secondaryPhone 
-parseInt (secondaryPhone) 
+const taxAsDecimal = tax / 100
+const startingAfterTax = salary * 1 - taxAsDecimal
+const balance = startingAfterTax - transport - food - rent
 
-console.log('Primary phone is valid numerical string:', primaryValid)
-console.log('Secondary phone is valid numerical string:', secondaryValid )
+if (hourOfDay && minuteOfDay !== null)  (hourOfDay == '00') && (minuteOfDay == '00')
+
+
+console.log(balance.toFixed(3))
